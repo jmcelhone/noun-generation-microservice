8f176e4ed3eb722c5481aa3b296129135ea9c5d1
Jeremy McElhone <42725080+jmcelhone@users.noreply.github.com>
2025-11-22 17:51:03 -0800

    Remove unecessary comments
    Refactor noun_generator.py to remove unused variable and improve readability.

--- noun_generator.py
+++ noun_generator.py
@@ -1,23 +1,16 @@
 import zmq
 import random
 
-# Setup ZMQ
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-# Save nouns into lists
 with open("list.txt", 'r') as f: random_lines = f.readlines()
-
 with open("animals.txt", 'r') as f: animal_lines = f.readlines()
-
 with open("feelings.txt", 'r') as f: feeling_lines = f.readlines()
-
 with open("food.txt", 'r') as f: food_lines = f.readlines()
-
 with open("vehicles.txt", 'r') as f: vehicle_lines = f.readlines()
 
-# Retrieve noun/s
 def get_noun(category):
     category = category.lower()
     if category == "animal":
@@ -39,19 +32,15 @@ def get_nouns(quantity, category = "random"):
         if i < int(quantity) - 1:
             noun_string += " "
     return noun_string
-
-# Send reply/ recieve request
+    
 while True:
     message = socket.recv()
-
     if len(message) > 0:
         s = message.decode().split()
         quantity = s[0]
-        
         if len(s) > 1:
             category = s[1]
         else:
             category = "random"
-
         nouns = get_nouns(quantity, category)
         socket.send_string(nouns)
